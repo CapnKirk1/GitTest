d86f31fefe4fdccb8ae361d62240daa9c08ef1ce
Seth Kirksey <sethkirksey2@gmail.com>
2016-03-02 18:21:58 -0500

    initial commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+    
+    public static void foo(){
+	System.out.println("git just got got");
+    }//foo
+
+    public static void bar(){
+	System.out.println("could've used this last week");
+    }//bar
+
+}//Hello
